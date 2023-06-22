83411639901ab4d473752ae7df70c2b2a483b02f
GershonWang <986432015@qq.com>
2023-06-22 17:33:26 +0800

    添加程序图标

--- src/main/java/com/dongpl/Main.java
+++ src/main/java/com/dongpl/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -21,6 +22,7 @@ public class Main extends Application {
         scene.getStylesheets().add("css/main.css");
         primaryStage.setScene(scene);
         primaryStage.setTitle("数据服务");
+        primaryStage.getIcons().add(new Image("images/logo.png"));
         primaryStage.show();
     }
 }
--- src/main/java/com/dongpl/controller/MainController.java
+++ src/main/java/com/dongpl/controller/MainController.java
@@ -111,6 +111,127 @@ public class MainController implements Initializable {
             alert.show();
             return;
         }
+        String parent = entity.getParentPath();
+        boolean isOpen = entity.isOpen();
+        String fileName = entity.getFileName();
+        String[] names = fileName.split(".properties");
+        String realName = names[0];
+        String serverName = realName.split("-")[1];
+        try {
+            String systemOS = System.getProperty("os.name");
+            System.out.println("当前系统版本：：" + systemOS);
+            if (systemOS.toLowerCase().contains("windows")) {
+                System.out.println("当前执行windows系统的操作");
+                if (isOpen) {
+                    // 获取系统进程列表 tasklist
+                    Process process = Runtime.getRuntime().exec("netstat -ano");//netstat -ano | findstr 11397
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("GBK")));
+                    String line;
+                    Pattern pattern = Pattern.compile("\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+");
+                    while ((line = reader.readLine()) != null) {
+                        Matcher matcher = pattern.matcher(line);
+                        if (matcher.find()) {
+                            String partStr = matcher.group().split(":")[1];
+                            if (Objects.equals(partStr, port)) {
+                                String pid = line.split("\\s")[5];
+                                if (!"0".equals(pid)) {
+                                    //根据进程id杀死进程
+                                    Process killProcess = Runtime.getRuntime().exec("taskkill /F /PID " + pid);
+                                    killProcess.waitFor();
+                                    // runtime正常输出
+                                    StringBuilder sb = new StringBuilder();
+                                    BufferedReader br = new BufferedReader(new InputStreamReader(killProcess.getInputStream(), Charset.forName("GBK")));
+                                    String linee;
+                                    while ((linee = br.readLine()) != null) {
+                                        textArea.setText(""); //每次打印前清空文本域
+                                        sb.append(linee).append("\n");
+                                        String body = sb.toString();
+                                        String cn = UnicodeBackslashU.unicodeToCn(body);
+                                        cn += "\"" + serverName.toUpperCase() + "\"服务停止成功！！！";
+                                        textArea.setText(cn);
+                                        button.setStyle("-fx-background-color: green;");
+                                        entity.setOpen(false);
+                                    }
+                                    // runtime出现异常
+                                    StringBuilder errorSB = new StringBuilder();
+                                    BufferedReader errorBR = new BufferedReader(new InputStreamReader(killProcess.getErrorStream(), Charset.forName("GBK")));
+                                    String errorLine;
+                                    while ((errorLine = errorBR.readLine()) != null) {
+                                        textArea.setText(""); //每次打印前清空文本域
+                                        errorSB.append(errorLine).append("\n");
+                                        String body = errorSB.toString();
+                                        String cn = UnicodeBackslashU.unicodeToCn(body);
+                                        cn += "\"" + serverName.toUpperCase() + "\"服务停止异常！！！";
+                                        textArea.setText(cn);
+                                        button.setStyle("-fx-background-color: red;");
+                                        entity.setOpen(true);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    String[] cdRoom = new String[] {"cmd","/C","cd /d " + parent + " && java -jar -Dspring.profiles.active=" + serverName + " emss-common-data-2.0.6.jar"};
+                    Process process = Runtime.getRuntime().exec(cdRoom);
+                    // 获取子进程的输入流和错误流
+                    InputStream inputStream = process.getInputStream();
+                    InputStream errorStream = process.getErrorStream();
+                    // 开启两个线程
+                    new Thread(() -> {
+                        try {
+                            long millis = System.currentTimeMillis();
+                            entity.setStartTime(millis);
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("GBK")));
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                System.out.println(line);
+                                // TODO 写入日志文件吧，直接写入界面会因为加载导致异常崩溃
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).start();
+                    new Thread(() -> {
+                        try {
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(errorStream, Charset.forName("GBK")));
+                            String line;
+                            while ((line = reader.readLine()) != null) {
+                                System.out.println(line);
+                                textArea.setText(line);
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }).start();
+                    textArea.setText("\"" + serverName.toUpperCase() + "\"服务启动成功！！！");
+                    button.setStyle("-fx-background-color: red;");
+                    entity.setOpen(true);
+                }
+            } else if (systemOS.toLowerCase().contains("linux")){
+                System.out.println("当前执行linux系统的操作");
+                if (isOpen) {
+                    System.out.println("执行关闭服务操作");
+                    button.setStyle("-fx-background-color: green;");
+                    entity.setOpen(false);
+                } else {
+                    System.out.println("执行开启服务操作");
+//                    String[] cdRoom = new String[]{"/bin/sh","-C","xterm -e cd " + parent};
+                    String[] cdRoom = new String[]{"/bin/sh","-C","cd " + parent + " && ls -l"};
+                    Process process = Runtime.getRuntime().exec(cdRoom);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("GBK")));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
+                    button.setStyle("-fx-background-color: red;");
+                    entity.setOpen(true);
+                }
+            } else {
+                System.out.println("当前是系统非主流版本");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
